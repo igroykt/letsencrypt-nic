6f69dc349528257360537fd7558517913c52307e
igro <admin@mytona.com>
2020-09-14 07:45:18 +0200

    minor bugfix; added logging

--- auth.py
+++ auth.py
@@ -3,8 +3,8 @@ import sys
 from nic_api import DnsApi
 from nic_api.models import TXTRecord
 from configparser import ConfigParser
-import ast
 import time
+import logging
 
 config = ConfigParser()
 try:
@@ -18,48 +18,63 @@ CLIENT_ID = os.getenv('NICID')
 CLIENT_SECRET = os.getenv('NICSECRET')
 TOKEN_FILE = config.get('GENERAL', 'TOKEN_FILE')
 SERVICE_ID = config.get('GENERAL', 'SERVICE_ID')
+LOG_FILE = config.get('GENERAL', 'LOG_FILE')
 TTL = config.get('GENERAL', 'TTL')
 SLEEP = int(config.get('GENERAL', 'SLEEP'))
 CERTBOT_DOMAIN = os.getenv('CERTBOT_DOMAIN')
 CERTBOT_VALIDATION = os.getenv('CERTBOT_VALIDATION')
 
-oauth_config = {
-    'APP_LOGIN': CLIENT_ID,
-    'APP_PASSWORD': CLIENT_SECRET
-}
+logging.basicConfig(format = '%(levelname)-8s [%(asctime)s] %(message)s', level = logging.ERROR, filename = LOG_FILE)
+
 try:
-    api = DnsApi(oauth_config)
+	oauth_config = {
+    		'APP_LOGIN': CLIENT_ID,
+    		'APP_PASSWORD': CLIENT_SECRET
+	}
 except Exception as err:
-    sys.exit(f"OAuth authentication: {err}")
+	logging.error(f"auth.py - oauth_config error: {err}")
+
+try:
+	api = DnsApi(oauth_config)
+except Exception as err:
+	logging.error(f"auth.py - DnsApi error: {err}")
 
 if not os.path.exists(TOKEN_FILE):
     open(TOKEN_FILE, 'w').close()
 
 try:
-    api.authorize(
-        username = USERNAME,
-        password = PASSWORD,
-        token_filename = TOKEN_FILE
-    )
+	api.authorize(
+        	username = USERNAME,
+        	password = PASSWORD,
+        	token_filename = TOKEN_FILE
+    	)
 except Exception as err:
-    print(f"API authorize: {err}")
-    print(f"Token refresh...")
-    os.remove(TOKEN_FILE)
-    api.authorize(
-        username = USERNAME,
-        password = PASSWORD,
-        token_filename = TOKEN_FILE
-    )
+	print(f"API authorize: {err}")
+	print(f"Token refresh...")
+	logging.error(f"auth.py - api.authorize error: {err}")
+	os.remove(TOKEN_FILE)
+	api.authorize(
+        	username = USERNAME,
+        	password = PASSWORD,
+        	token_filename = TOKEN_FILE
+    	)
 
 try:
-    print("Creating TXT record...")
-    record = TXTRecord(name = f"_acme-challenge.{CERTBOT_DOMAIN}.", txt = CERTBOT_VALIDATION, ttl = TTL)
-except Exception:
-    os.remove(TOKEN_FILE)
-    record = TXTRecord(name = f"_acme-challenge.{CERTBOT_DOMAIN}.", txt = CERTBOT_VALIDATION, ttl = TTL)
+	print("Creating TXT record...")
+	record = TXTRecord(name = f"_acme-challenge.{CERTBOT_DOMAIN}.", txt = CERTBOT_VALIDATION, ttl = TTL)
+except Exception as err:
+	logging.error(f"auth.py - TXTRecord error: {err}")
+	record = TXTRecord(name = f"_acme-challenge.{CERTBOT_DOMAIN}.", txt = CERTBOT_VALIDATION, ttl = TTL)
 
-api.add_record(record, SERVICE_ID, CERTBOT_DOMAIN)
-api.commit(SERVICE_ID, CERTBOT_DOMAIN)
+try:
+	api.add_record(record, SERVICE_ID, CERTBOT_DOMAIN)
+except Exception as err:
+	logging.error(f"auth.py - api.add_record error: {err}")
+
+try:
+	api.commit(SERVICE_ID, CERTBOT_DOMAIN)
+except Exception as err:
+	logging.error(f"auth.py - api.commit error: {err}")
 
 print(f"Sleep for {SLEEP} seconds...")
 time.sleep(SLEEP)
--- clean.py
+++ clean.py
@@ -3,6 +3,7 @@ import sys
 from nic_api import DnsApi
 from nic_api.models import TXTRecord
 from configparser import ConfigParser
+import logging
 
 config = ConfigParser()
 try:
@@ -18,41 +19,47 @@ TOKEN_FILE = config.get('GENERAL', 'TOKEN_FILE')
 SERVICE_ID = config.get('GENERAL', 'SERVICE_ID')
 CERTBOT_DOMAIN = os.getenv('CERTBOT_DOMAIN')
 
-oauth_config = {
-    'APP_LOGIN': CLIENT_ID,
-    'APP_PASSWORD': CLIENT_SECRET
-}
+logging.basicConfig(format = '%(levelname)-8s [%(asctime)s] %(message)s', level = logging.ERROR, filename = LOG_FILE)
+
+try:
+	oauth_config = {
+    		'APP_LOGIN': CLIENT_ID,
+    		'APP_PASSWORD': CLIENT_SECRET
+	}
+except Exception as err:
+	logging.error(f"clean.py - oauth_config error: {err}")
+
 try:
     api = DnsApi(oauth_config)
 except Exception as err:
-    sys.exit(err)
+    logging.error(f"clean.py - DnsApi error: {err}")
 
 if not os.path.exists(TOKEN_FILE):
     open(TOKEN_FILE, 'w').close()
 
 try:
-    api.authorize(
-        username = USERNAME,
-        password = PASSWORD,
-        token_filename = TOKEN_FILE
-    )
+	api.authorize(
+        	username = USERNAME,
+        	password = PASSWORD,
+        	token_filename = TOKEN_FILE
+    	)
 except Exception as err:
-    print(f"API authorize: {err}")
-    print(f"Token refresh...")
-    os.remove(TOKEN_FILE)
-    api.authorize(
-        username = USERNAME,
-        password = PASSWORD,
-        token_filename = TOKEN_FILE
-    )
+    	print(f"API authorize: {err}")
+    	print(f"Token refresh...")
+	logging.error(f"clean.py - api.authorize error: {err}")
+    	os.remove(TOKEN_FILE)
+    	api.authorize(
+        	username = USERNAME,
+        	password = PASSWORD,
+        	token_filename = TOKEN_FILE
+    	)
 
 try:
-    print("Retrieving DNS records...")
-    records = api.records(SERVICE_ID, CERTBOT_DOMAIN)
+    	print("Retrieving DNS records...")
+    	records = api.records(SERVICE_ID, CERTBOT_DOMAIN)
 except Exception:
-    # refresh token if expired
-    os.remove(TOKEN_FILE)
-    records = api.records(SERVICE_ID, CERTBOT_DOMAIN)
+	logging.error(f"clean.py - api.records error: {err}")
+    	records = api.records(SERVICE_ID, CERTBOT_DOMAIN)
 
 def findTXTID(data):
     ids = []
@@ -70,6 +77,14 @@ def findTXTID(data):
 print("Searching TXT record ID...")
 record_id = findTXTID(records)
 for id in record_id:
-    api.delete_record(id, SERVICE_ID, CERTBOT_DOMAIN)
-api.commit(SERVICE_ID, CERTBOT_DOMAIN)
+	try:
+    		api.delete_record(id, SERVICE_ID, CERTBOT_DOMAIN)
+	except Exception as err:
+		logging.error(f"clean.py - api.delete error: {err}")
+
+try:
+	api.commit(SERVICE_ID, CERTBOT_DOMAIN)
+except Exception as err:
+	logging.error(f"clean.py - api.commit error: {err}")
+
 print("Done!")
