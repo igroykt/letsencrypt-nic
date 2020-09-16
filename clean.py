f1d64b4e1187bf4ce739d74a1b6cd89d6f29f5b7
igro <admin@mytona.com>
2020-09-16 11:08:30 +0200

    fixed tabs; fixed exception; logic update

--- clean.py
+++ clean.py
@@ -7,9 +7,9 @@ import logging
 
 config = ConfigParser()
 try:
-    config.read('config.ini')
+	config.read('config.ini')
 except Exception as err:
-    sys.exit(f"Config parse: {err}")
+	sys.exit(f"Config parse: {err}")
 
 USERNAME = os.getenv('NICUSER')
 PASSWORD = os.getenv('NICPASS')
@@ -24,26 +24,26 @@ logging.basicConfig(format = '%(levelname)-8s [%(asctime)s] %(message)s', level 
 
 try:
 	oauth_config = {
-    		'APP_LOGIN': CLIENT_ID,
-    		'APP_PASSWORD': CLIENT_SECRET
+		'APP_LOGIN': CLIENT_ID,
+		'APP_PASSWORD': CLIENT_SECRET
 	}
 except Exception as err:
 	logging.error(f"clean.py - oauth_config error: {err}")
 
 try:
-    api = DnsApi(oauth_config)
+	api = DnsApi(oauth_config)
 except Exception as err:
-    logging.error(f"clean.py - DnsApi error: {err}")
+	logging.error(f"clean.py - DnsApi error: {err}")
 
 if not os.path.exists(TOKEN_FILE):
-    open(TOKEN_FILE, 'w').close()
+	open(TOKEN_FILE, 'w').close()
 
 try:
 	api.authorize(
-        	username = USERNAME,
-        	password = PASSWORD,
-        	token_filename = TOKEN_FILE
-    	)
+		username = USERNAME,
+		password = PASSWORD,
+		token_filename = TOKEN_FILE
+	)
 except Exception as err:
 	print(f"API authorize: {err}")
 	print(f"Token refresh...")
@@ -56,32 +56,32 @@ except Exception as err:
 	)
 
 try:
-    	print("Retrieving DNS records...")
-    	records = api.records(SERVICE_ID, CERTBOT_DOMAIN)
-except Exception:
-	logging.error(f"clean.py - api.records error: {err}")
+	print("Retrieving DNS records...")
 	records = api.records(SERVICE_ID, CERTBOT_DOMAIN)
+except Exception as err:
+	logging.error(f"clean.py - api.records error: {err}")
 
 def findTXTID(data):
-    ids = []
-    for record in data:
-        # skip all records except TXT
-        if type(record) is TXTRecord:
-            # convert TXTRecord type to string
-            record = repr(record)
-            # convert string to dictionary
-            record = eval(record)
-            if f"_acme-challenge" in record['name']:
-                ids.append(record['id'])
-    return ids
+	for record in data:
+		# skip all records except TXT
+		if type(record) is TXTRecord:
+			# convert TXTRecord type to string
+			record = repr(record)
+			# convert string to dictionary
+			record = eval(record)
+			if "_acme-challenge" in record['name']:
+				return record['id']
 
 print("Searching TXT record ID...")
-record_id = findTXTID(records)
-for id in record_id:
-	try:
-    		api.delete_record(id, SERVICE_ID, CERTBOT_DOMAIN)
-	except Exception as err:
-		logging.error(f"clean.py - api.delete error: {err}")
+try:
+	record_id = findTXTID(records)
+except Exception as err:
+	logging.error(f"clean.py - findTXTID error: {err}")
+
+try:
+	api.delete_record(record_id, SERVICE_ID, CERTBOT_DOMAIN)
+except Exception as err:
+	logging.error(f"clean.py - api.delete error: {err}")
 
 try:
 	api.commit(SERVICE_ID, CERTBOT_DOMAIN)
