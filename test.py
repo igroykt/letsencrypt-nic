28ef9edcba5681b740cd987bf228b924f799fd51
igroykt <artofmotion.igroman@gmail.com>
2021-09-27 20:14:19 +0900

    test update

--- test.py
+++ test.py
@@ -4,17 +4,21 @@ import os
 import pytest
 from func import Func
 
-result = Func.checkTXTRecord(['8.8.8.8'], 'google.com', test=True)
-assert isinstance(result, bool)
+def test_checkTXTRecord():
+    result = Func.checkTXTRecord(['8.8.8.8'], 'google.com', test=True)
+    assert isinstance(result, bool)
 
-result = Func.mainDomainTail('test.google.com')
-assert result == 'google.com'
+def test_mainDomainTail():
+    result = Func.mainDomainTail('top.level.mydomain.ru')
+    assert result == 'mydomain.ru'
 
-result = Func.NIC_findTXTID([{'name':'_acme-challenge.google.com', 'id':'google.com'}])
-assert result == ['google.com']
+def test_NIC_findTXTID():
+    result = Func.NIC_findTXTID([{'name':'_acme-challenge.google.com', 'id':'google.com'}])
+    assert result == ['google.com']
 
-code, out, err = Func.call('ls')
-assert code == 0
+def test_call():
+    code, out, err = Func.call('ls')
+    assert code == 0
 
 '''
 result = Func.sendEmail(
@@ -31,20 +35,22 @@ result = Func.sendEmail(
 assert isinstance(result, bool)
 '''
 
-result = Func.makeList(['mydomain.ru', '*.mydomain.ru', 'mydomain2.ru'])
-assert result == '-d mydomain.ru -d *.mydomain.ru -d mydomain2.ru'
+def test_makeList():
+    result = Func.makeList(['mydomain.ru', '*.mydomain.ru', 'mydomain2.ru'])
+    assert result == '-d mydomain.ru -d *.mydomain.ru -d mydomain2.ru'
 
-result = Func.makeMainDomain(['mydomain.ru', '*.mydomain.ru', 'mydomain2.ru'])
-assert result == 'mydomain.ru'
+def test_makeMainDomain():
+    result = Func.makeMainDomain(['mydomain.ru', '*.mydomain.ru', 'mydomain2.ru'])
+    assert result == 'mydomain.ru'
 
 ENC_KEY = '0IFzRIVb4i42OPaovw0RDHNgOiRsKLlyDumAW_xFs0M='
 script_dir = os.path.dirname(os.path.realpath(__file__))
 ENC_DAT = f'{script_dir}/test_enc.dat'
-result = Func.encrypt(ENC_KEY, ENC_DAT, 'testuser', 'testpass', 'testid', 'testsecret')
-assert isinstance(result, bool)
 
-username, password, client_id, client_secret = Func.decrypt(ENC_KEY, ENC_DAT)
-assert username == 'testuser'
+def test_encrypt():
+    result = Func.encrypt(ENC_KEY, ENC_DAT, 'testuser', 'testpass', 'testid', 'testsecret')
+    assert isinstance(result, bool)
 
-result = Func.mainDomainTail('top.level.mydomain.ru')
-assert result == 'mydomain.ru'
+def test_decrypt():
+    username, password, client_id, client_secret = Func.decrypt(ENC_KEY, ENC_DAT)
+    assert username == 'testuser'
